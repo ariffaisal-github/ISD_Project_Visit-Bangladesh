743d4f0b435939168d59de6aae0ad4d037bbf935
ariffaisal-github <1805118@ugrad.cse.buet.ac.bd>
2023-02-16 22:01:10 +0600

    Post HTML Form Data to Nodejs server

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static("public"))
 app.get("/", (req, res) => {
     res.status(200).render("index", { pageTitle: "Homepage" });
@@ -10,6 +12,12 @@ app.get("/", (req, res) => {
 app.get("/day_by_day", (req, res) => {
     res.status(200).render("day_by_day", { pageTitle: "Day by Day" });
 });
+
+app.post("/location_entered", (req, res) => {
+    console.log(req.body); // access the form data using the 'name' attribute of the input field
+    res.redirect("/day_by_day"); // redirect to another page
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
